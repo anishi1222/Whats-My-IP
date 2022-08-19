dad0d3f38ee21ad8c081186ab34d1793ca1c592e
anishi1222 <a_nishi-anarch@bc4.so-net.ne.jp>
2022-08-19 18:52:10 +0900

    Environment.java is modified

--- src/main/java/io/logicojp/example/micronaut/Environment.java
+++ src/main/java/io/logicojp/example/micronaut/Environment.java
@@ -1,2 +1,17 @@
-package io.logicojp.example.micronaut;public class Environment {
+package io.logicojp.example.micronaut;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import java.util.Map;
+import java.util.Optional;
+
+@Controller("/env")
+public class Environment {
+
+    @Get(produces = MediaType.APPLICATION_JSON)
+    Optional<Map<String, String>> environment() {
+        // Return Environment variables defined in the environment simplily
+        return Optional.of(System.getenv());
+    }
 }
