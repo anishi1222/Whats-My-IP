b9a040e8f33abcd9105a53f6a191c3290247f171
Akihiro Nishikawa <11570959+anishi1222@users.noreply.github.com>
2022-10-28 11:08:38 +0900

    Create GeoIPClient.java

---
+++ src/main/java/io/logicojp/example/micronaut/GeoIPClient.java
@@ -0,0 +1,14 @@
+package io.logicojp.examples.micronaut;
+import org.reactivestreams.Publisher;
+
+import io.micronaut.core.async.annotation.SingleResult;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.client.annotation.Client;
+
+@Client("https://ip.seeip.org/geoip")
+public interface GeoIPClient  {
+    @Get(consumes = MediaType.APPLICATION_JSON)
+    @SingleResult
+    Publisher<String> getMyIpText();
+}
