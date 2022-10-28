885cf58c2da4d46263f1f8120209224d4c179a24
Akihiro Nishikawa <11570959+anishi1222@users.noreply.github.com>
2022-10-28 11:08:58 +0900

    Create GeoIPController.java

---
+++ src/main/java/io/logicojp/example/micronaut/GeoIPController.java
@@ -0,0 +1,21 @@
+package io.logicojp.examples.micronaut;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import org.reactivestreams.Publisher;
+
+@Controller("/caller")
+public class GeoIPController {
+
+    private final GeoIPClient geoIPClient;
+
+    public GeoIPController(GeoIPClient geoIPClient) {
+        this.geoIPClient = geoIPClient;
+    }
+
+    @Get(produces = MediaType.APPLICATION_JSON)
+    Publisher<String> sayHello() {
+        return geoIPClient.getMyIpText();
+    }
+}
