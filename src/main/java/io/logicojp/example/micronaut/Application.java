1a34c9515f99d7a4f6dc1c445316c052abfe9c01
Akihiro Nishikawa <11570959+anishi1222@users.noreply.github.com>
2022-10-28 11:07:42 +0900

    Update Application.java

--- src/main/java/io/logicojp/example/micronaut/Application.java
+++ src/main/java/io/logicojp/example/micronaut/Application.java
@@ -1,9 +1,7 @@
 package io.logicojp.example.micronaut;
 
-import io.micronaut.http.annotation.Controller;
 import io.micronaut.runtime.Micronaut;
 
-@Controller("/env")
 public class Application {
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
